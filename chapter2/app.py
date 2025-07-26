01a3f01c07d4902edc850dc9d451bd9ee63fc5ca
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-26 17:46:14 +0200

    learn about formatted string in python

--- chapter2/app.py
+++ chapter2/app.py
@@ -23,3 +23,18 @@ print(greeting.replace('ninjas', 'garvin'))
 making comments
 print()
 '''
+
+# fromatted strings (f-strings)
+name = 'mario'
+score = 50
+
+# examples - newer
+# print(f'{name} scored {score} points')
+
+# older approach
+print("{} has a score of {}".format(name, score))
+print("{0} has a score of {1}".format(name, score))
+print("{n} has a score of {s}".format(n=name, s=score))
+
+# expressions in f-strings
+print(f"{name.capitalize()} scored {score + 10} points")
