88e3537da8bc7fcf144a36f49e17f9750a633cc8
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-26 19:55:47 +0200

    learn while loops in python

---
+++ chapter3/while_loops.py
@@ -0,0 +1,13 @@
+# while loops
+
+score = 0
+
+# while score <= 10:
+#   print('Current score:', score)
+#   score += 1
+  
+# user_input = ''
+
+while user_input != 'q':
+  user_input = input('Enter a command (q to quit): ')
+  print('You entered:', user_input)
