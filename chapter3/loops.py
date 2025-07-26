d20b0c1bde43142e7c5d774c138bdaa87cecadf3
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-26 20:11:05 +0200

    learn about break and continue

---
+++ chapter3/loops.py
@@ -0,0 +1,36 @@
+# while loops
+'''
+score = 0
+
+# while score <= 10:
+#   print('Current score:', score)
+#   score += 1
+  
+# user_input = ''
+
+while user_input != 'q':
+  user_input = input('Enter a command (q to quit): ')
+  print('You entered:', user_input)
+
+
+# Break and Continue
+count  = 0
+
+# break
+# break out of the loop
+
+while count < 10:
+  if count == 5:
+    # print('Breaking out of the loop at count:', count)
+    break
+  # print(count)
+  count += 1
+
+
+# continue
+while count < 10:
+  count += 1
+  if count % 2 != 0:
+    print(count)
+    continue
+'''
