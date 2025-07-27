93341199133fcd252fb5dbb438998fbe725dae17
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-27 20:46:34 +0200

    elarn about errorHandling

--- chapter3/match_cases.py
+++ chapter3/match_cases.py
@@ -1,5 +1,5 @@
 # match case statements
-
+'''
 belt_color = input('what is your belt color: ')
 
 match belt_color: # like switch case in other languages
@@ -16,3 +16,29 @@ match belt_color: # like switch case in other languages
   case _:
     print('Unknown belt color.')
 # The underscore (_) acts as a default case, similar to 'default' in switch statements.
+'''
+# Error handling
+
+number = int(input('Enter a number: '))
+
+# example 1
+# try:
+#   number = int(input('Enter a number: '))
+#   print('you entered:', number)
+# except ValueError as e:
+#   print('That is not a valid number.')
+#   print('Error:', e)
+# finally:
+#   print('completed')
+
+# example 2
+a = 10
+b = 0
+
+try:
+  print(a / b)
+except ZeroDivisionError as e:
+  print('Error: Division by zero is not allowed.')
+  print('error:', e)
+
+print('end of the file')
