c6e3e9c7bc8cfcf2998abdf50a15cffa6b01619f
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-28 22:27:38 +0200

    learn about variable scope

--- chapter4/functions.py
+++ chapter4/functions.py
@@ -5,9 +5,28 @@
 
 # greet()
 
-def say_hello(name):
+'''def say_hello(name):
   name = 'mario'
   return 'hello ' + name
 
 result = say_hello('mario')
-print(result)
+print(result)'''
+
+# variable scope
+
+# gloabal & local variables
+
+x = 10
+
+'''def print_x():
+  global x  # points to the global variable x
+  x = 5  # this is within the function scope
+  print(f'x inside the print_x function: {x}')
+  
+def print_y():
+  y = 20
+  print(f'y inside the print_y function: {y}')
+
+print_x()
+print(f'global_value of x: {x}')
+print_y()'''
