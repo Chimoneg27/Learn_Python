49f119d537dc1c8f9f7feca2cdce3c6a482933f1
Chimoneg27 <chimonegarvin27@gmail.com>
2025-07-28 22:10:37 +0200

    basics on funnctions in python

---
+++ chapter4/functions.py
@@ -0,0 +1,13 @@
+# defining functions
+
+# def greet():
+  # print('hello ninjas')
+
+# greet()
+
+def say_hello(name):
+  name = 'mario'
+  return 'hello ' + name
+
+result = say_hello('mario')
+print(result)
