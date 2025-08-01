6649fc6c0a4fd8e6d6c42c6b14b605db6d98da59
Chimoneg27 <chimonegarvin27@gmail.com>
2025-08-02 00:04:01 +0200

    write and read json data

--- chapter5/app.py
+++ chapter5/app.py
@@ -1,16 +1,36 @@
 from pathlib import Path
+import json
 
-def open_file():
-  path = Path(__file__).parent / 'characters.txt'
-  data = ['mario', 'luigi', 'peach', 'bowser', 'toad']
-  
-  # context managers --> automatically close the file
+path = Path(__file__).parent / 'characters.json'
+
+
+characters = {
+    "characters": [
+      {'name': "Mario", 'age': 25},
+      {'name': "Luigi", 'age': 24},
+      {'name': "Peach", 'age': 22},
+      {'name': "Bowser", 'age': 30},
+      {'name': "Toad", 'age': 20},
+      {'name': "Yoshi", 'age': 21},
+      {'name': "Wario", 'age': 28},
+      {'name': "Waluigi", 'age': 27}
+    ]
+  }
+
+def write_json():
   with path.open('w') as file:
-    for character in data:
-      file.write(character + '\n')
+    json.dump(characters, file, indent=2)
+  return
+
+def read_json():
+  with path.open('r') as file:
+    data = json.load(file)
+  return data
 
 def main():
-  open_file()
+  write_json()
+  data = read_json()
+  print(data)
 
 if __name__ == "__main__":
   main()
