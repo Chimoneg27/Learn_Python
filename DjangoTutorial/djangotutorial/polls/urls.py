a17b34d6c97073a74ecc63caf932da5c54458f06
Chimoneg27 <chimonegarvin27@gmail.com>
2026-02-09 16:01:34 +0200

    learn about views in django

--- DjangoTutorial/djangotutorial/polls/urls.py
+++ DjangoTutorial/djangotutorial/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # ex: /polls/ main page
   path('', views.index, name='index'),
+  # example /polls/5/ detail page for question id 5
+  path('<int:question_id>/', views.detail, name='detail'),
+  # example /polls/5/results/ results page for question id 5
+  path('<int:question_id>/results/', views.results, name='results'),
+  # example /polls/5/vote/ vote page for question id 5
+  path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
