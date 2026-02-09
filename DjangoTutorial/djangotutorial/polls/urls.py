d86bfe98f648eef6cb2dbd1f0796d9a580a8e39b
Chimoneg27 <chimonegarvin27@gmail.com>
2026-02-09 16:51:11 +0200

    learn about templates and url patterns

--- DjangoTutorial/djangotutorial/polls/urls.py
+++ DjangoTutorial/djangotutorial/polls/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
   # ex: /polls/ main page
   path('', views.index, name='index'),
   # example /polls/5/ detail page for question id 5
-  path('<int:question_id>/', views.detail, name='detail'),
+  path('/<int:question_id>/', views.detail, name='detail'),
   # example /polls/5/results/ results page for question id 5
   path('<int:question_id>/results/', views.results, name='results'),
   # example /polls/5/vote/ vote page for question id 5
