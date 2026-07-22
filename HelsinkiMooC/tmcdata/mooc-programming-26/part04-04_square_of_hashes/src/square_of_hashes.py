c9f1c28394d42ccb65f30a943381529c2aec0ffe
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-22 20:32:21 +0200

    complete square of hashes exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-04_square_of_hashes/src/square_of_hashes.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,9 +1,12 @@
 # Copy here code of line function from previous exercise
+def line(num, hash):
+    print(num * hash)
 
 def square_of_hashes(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    for i in range(size):
+        line(size, '#')
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    square_of_hashes(5)
+    square_of_hashes(3)
