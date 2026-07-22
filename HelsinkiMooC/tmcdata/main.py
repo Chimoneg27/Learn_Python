30749b9dd6ce7bc35ac2f2c4b33bd1df3ef63ca2
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-22 19:56:12 +0200

    add uv python

---
+++ HelsinkiMooC/tmcdata/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from tmcdata!")
+
+
+if __name__ == "__main__":
+    main()
--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,11 @@
 # Write your solution here
+while True:
+  user_input = input('Editor: ')
+  
+  if user_input.lower() == 'visual studio code':
+    print('an excellent choice!')
+    break
+  if user_input.lower() == 'word' or user_input.lower() == 'notepad':
+    print('awful')
+  else:
+    print('not good')
