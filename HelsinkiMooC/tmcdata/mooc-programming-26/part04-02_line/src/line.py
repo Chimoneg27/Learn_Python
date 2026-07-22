283e3c561ff6a55793622ed0f4f5a255baf57395
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-22 20:09:25 +0200

    complete line exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-02_line/src/line.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-02_line/src/line.py
@@ -1,4 +1,9 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def line(num, letter):
+    if len(letter) == 0:
+        letter = '*'
+    print(num * letter[0])
+
 if __name__ == "__main__":
     line(5, "x")
