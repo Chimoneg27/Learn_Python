84678357e89b27a94f6c0b0974840505e418e353
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-24 17:27:48 +0200

    finish a spruce exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-08_spruce/src/spruce.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-08_spruce/src/spruce.py
@@ -1,4 +1,12 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def spruce(size):
+    print('a spruce!')
+    for i in range(size):
+        spaces = size - 1 - i
+        stars = 2 * i + 1
+        print(' ' * spaces + '*' * stars)
+    print(' ' * (size - 1) + '*')
+        
 if __name__ == "__main__":
     spruce(5)
