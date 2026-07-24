4bfb770be565111474845b0a1ac89cded8a06294
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-24 16:42:01 +0200

    finish triangle exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-06_triangle/src/triangle.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-06_triangle/src/triangle.py
@@ -1,8 +1,13 @@
 # Copy here code of line function from previous exercise
+def line(num, hash):
+    print(num * hash)
 
 def triangle(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    num = 1
+    while num <= size:
+      line(num, "#")
+      num += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
