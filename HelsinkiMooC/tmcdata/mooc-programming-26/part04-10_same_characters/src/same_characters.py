2834155938887ae87d5884fe595c9c0d2ae6a136
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-02 20:38:24 +0200

    finish same_characters exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-10_same_characters/src/same_characters.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,11 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def same_chars(str, num1, num2):
+    # print(str[num1] == str[num2])
+    if num2 >= len(str) or num1 >= len(str):
+        return False
+    
+    return str[num1] == str[num2]
+
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    print(same_chars("coder", 1, 10))
