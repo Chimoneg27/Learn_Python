8e13cee9f01c0844d3e355b848749d7c0bd3a568
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-04 16:43:12 +0200

    finish change value of item exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-12_change_value_of_item/src/change_value_of_item.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+arr = [1,2,3,4,5]
+while True:
+  num = int(input('Index: '))
+  
+  if num == -1:
+    break
+  
+  new_val = int(input("New value: "))
+
+  if num < 0 or num >= len(arr):
+    print("Index is out the range of the list")
+    continue
+
+  arr[num] = new_val
+  print(arr)
