d0bab25843905d34110d7e31fe4c5bb508116c32
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-05 16:55:25 +0200

    finish negative_to_positive exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-21_negative_to_positive/src/negative_to_positive.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-21_negative_to_positive/src/negative_to_positive.py
@@ -1,1 +1,9 @@
 # Write your solution here
+nums = int(input('Please type in a positive integer: '))
+neg = -1 * nums
+end = nums + 1
+
+for i in range(neg, end, 1):
+  if i == 0:
+    continue
+  print(i)
