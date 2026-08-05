56fabd10bae4e6b830c7a84ff4fc6700f9e8a545
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-05 16:30:30 +0200

    finish list_twice exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-16_list_twice/src/list_twice.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,15 @@
 # Write your solution here
+
+arr = []
+
+while True:
+  item = int(input('New item: '))
+  
+  if item == 0:
+    print('Bye!')
+    break
+
+  arr.append(item)
+  print(f'The list now: {arr}')
+  sort_arr = sorted(arr)
+  print(f'The list in order: {sort_arr}')
