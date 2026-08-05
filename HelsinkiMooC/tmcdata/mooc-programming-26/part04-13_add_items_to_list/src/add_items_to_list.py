6d343c1e0a5688f726dffc08286e09d725cf7aca
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-05 14:41:18 +0200

    finish add_items_to_list exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-13_add_items_to_list/src/add_items_to_list.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+arr = []
+size = int(input('How many items: '))
+while size > 0:
+  item_num = 1
+  new_item = int(input(f'Item {item_num}: '))
+  arr.append(new_item)
+
+  size -= 1
+  # item_num += 1
+print(arr)
