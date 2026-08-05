7ee781a2857d85c0639c7d257def150caea66f44
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-05 16:32:23 +0200

    finish length_of_list

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-17_length_of_list/src/length_of_list.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-17_length_of_list/src/length_of_list.py
@@ -1,5 +1,9 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def length(arr):
+    size = len(arr)
+    return size
+
 if __name__ == "__main__":
     my_list = [3, 6, -4]
     result = length(my_list)
