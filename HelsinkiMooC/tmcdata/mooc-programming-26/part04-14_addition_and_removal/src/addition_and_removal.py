e95be9bac0535eabd8c1db71980db38069dd8305
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-05 15:16:09 +0200

    finish addition_and_removal exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-14_addition_and_removal/src/addition_and_removal.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-14_addition_and_removal/src/addition_and_removal.py
@@ -1,1 +1,22 @@
 # Write your solution here
+
+arr = []
+print(f'The list is now {arr}')
+start = 0
+while True:
+
+  add_rem_exit = input('a(d)d, (r)emove or e(x)it:' )
+  if add_rem_exit == 'x':
+    print('Bye!')
+    break
+  elif add_rem_exit == 'd':
+    start += 1
+    arr.append(start)
+    print(f'The list is now {arr}')
+  elif add_rem_exit == 'r':
+    start -= 1
+    arr.pop()
+    print(f'The list is now {arr}')
+  else:
+    break
+  
