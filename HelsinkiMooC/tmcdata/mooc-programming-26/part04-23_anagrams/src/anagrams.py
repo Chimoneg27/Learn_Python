392c537ad535a6ea03913f6f83eebc21320d4d09
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 04:46:02 +0200

    finish anagram exercie

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-23_anagrams/src/anagrams.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def anagrams(wrd1, wrd2):
+   sort1 = sorted(wrd1)
+   sort2 = sorted(wrd2)
+   if sort1 == sort2:
+     return True
+   else:
+     return False
+
+if __name__ == "__main__":
+    print(anagrams("tame", "meta"))
+    print(anagrams("tame", "mate")) # True
+    print(anagrams("tame", "team")) # True
+    print(anagrams("tabby", "batty")) # False
