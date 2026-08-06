33f5758dc6b7cf22459f8345a487fbcfcfaa80b9
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 06:45:35 +0200

    finish length_of_longest exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-29_length_of_longest/src/length_of_longest.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def length_of_longest(arr):
+  num = []
+  for i in range(len(arr)):
+    num.append(len(arr[i]))
+  
+  longest = max(num)
+  return longest
+
+if __name__ == "__main__":
+  my_list = ["first", "second", "fourth", "eleventh"]
+
+  result = length_of_longest(my_list)
+  print(result)
