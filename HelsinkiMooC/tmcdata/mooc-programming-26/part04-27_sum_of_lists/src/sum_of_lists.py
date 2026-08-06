6f01df52fb4504827d41e22078125a277f68cdac
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 06:00:14 +0200

    finish sum_of_lists exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-27_sum_of_lists/src/sum_of_lists.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def list_sum(arr1, arr2):
+  combined = []
+  
+  for i in range(len(arr1)):
+    combined.append(arr1[i] + arr2[i])
+  return combined
+
+if __name__ == "__main__":
+  a = [1, 2, 3]
+  b = [7, 8, 9]
+  print(list_sum(a, b))
