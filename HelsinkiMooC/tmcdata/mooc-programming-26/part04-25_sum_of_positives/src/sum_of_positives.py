6efd23ef6fc9ac5d1c808f5f8a01940baa3fb101
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 05:39:35 +0200

    finish sum_of_positives exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-25_sum_of_positives/src/sum_of_positives.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def sum_of_positives(arr):
+  total = 0
+  for i in range(len(arr)):
+    if arr[i] > 0:
+      total += arr[i]
+  print(total)
+  return total
+
+if __name__ == "__main__":
+  sum_of_positives([1, -2, 3, -4, 5])
