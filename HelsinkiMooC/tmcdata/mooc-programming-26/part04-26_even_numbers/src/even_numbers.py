463c328de7122d54939deef6c07640ecbceabb4a
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 05:53:17 +0200

    finish even_numbers exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-26_even_numbers/src/even_numbers.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-26_even_numbers/src/even_numbers.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def even_numbers(arr):
+  even = []
+  for i in range(len(arr)):
+    if arr[i] % 2 == 0:
+      even.append(arr[i])
+  return even
+
+if __name__ == "__main__":
+  my_list = [1, 2, 3, 4, 5]
+  new_list = even_numbers(my_list)
+  print('original', my_list)
+  print('new', even_numbers(new_list))
