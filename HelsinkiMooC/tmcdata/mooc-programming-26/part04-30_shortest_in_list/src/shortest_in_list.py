98c310fbfe9a929ebf45a9c48fb375b18f3590f9
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 07:01:16 +0200

    finish shortest_in_list exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-30_shortest_in_list/src/shortest_in_list.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,25 @@
 # Write your solution here
+def shortest(arr):
+  num = []
+  for i in range(len(arr)):
+    num.append(len(arr[i]))
+  
+  short = min(num)
+  for item in arr:
+    if len(item) == short:
+      return item
+  # model solution
+  '''
+  result = ''
+  
+  for nimi in names:
+    if result == "" or len(nimi) < len(result):
+      result = nimi
+  return result
+  '''
+
+if __name__ == "__main__":
+  my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+  result = shortest(my_list)
+  print(result)
