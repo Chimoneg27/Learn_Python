dd3820c3eda3efcfd4e3ac16770ff084944bfa46
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 07:11:53 +0200

    finis all_longest_in_list exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def all_the_longest(arr):
+  longest_arr = []
+  longest = 0
+  
+  for name in arr:
+    if len(name) > longest:
+      longest = len(name)
+  
+  for i in range(len(arr)):
+    if len(arr[i]) == longest:
+      longest_arr.append(arr[i])
+  return longest_arr
+
+
+if __name__ == "__main__":
+  my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+  result = all_the_longest(my_list)
+  print(result) # ['dorothy', 'richard']
