29be4e6b2fa6360f395c629a5c31cc689a99a8aa
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-06 06:08:04 +0200

    finish distinct_numbers.py

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-28_distinct_numbers/src/distinct_numbers.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def distinct_numbers(arr):
+  new_set = set(arr)
+  distinct = list(new_set)
+  return sorted(distinct)
+
+if __name__ == "__main__":
+  my_list = [3, 2, 2, 1, 3, 3, 1]
+  print(distinct_numbers(my_list)) # [1, 2, 3]
+  print(distinct_numbers(([1, 10, 1, 100, 1, 1000])))
