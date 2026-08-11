30c58bb27e9c6c2b18dd0c8df7c9f75a5f2886ff
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-11 17:13:09 +0200

    finish most common character exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-34_most_common_character/src/most_common_character.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,23 @@
 # Write your solution here
+def most_common_character(str):
+  great = 0
+  top = ''
+  single = []
+  occur = []
+  for char in str:
+    single.append(char)
+  # return single
+  
+  for lett in single:
+    current = single.count(lett)
+    if current > great:
+      great = current
+      top = lett  
+  return top
+
+if __name__ == "__main__":
+  first_string = "abcdbde"
+  print(most_common_character(first_string))
+
+  second_string = "exemplaryelementary"
+  print(most_common_character(second_string))
