5d095fed3178363941488431bc8eeef0cd1b9cbe
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-11 16:34:38 +0200

    finish integers to strings exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-32_integers_to_strings/src/integers_to_strings.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def formatted(arr):
+  new_arr = []
+  for num in arr:
+    new_arr.append(f'{num:.2f}')
+  return new_arr
+
+if __name__ == "__main__":
+  my_list = [1.234, 0.3333, 0.11111, 3.446]
+  new_list = formatted(my_list)
+  print(new_list)
