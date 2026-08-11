d8bd08751aad693635550ee733b6d0bf3a757696
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-11 17:25:55 +0200

    finish no vowels allowed exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def no_vowels(str):
+  new_str = []
+  voweled = []
+  for lett in str:
+    new_str.append(lett)
+  
+  for char in new_str:
+    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+      continue
+    
+    voweled.append(char)
+  return "".join(voweled)
+
+if __name__ == "__main__":
+  my_string = "this is an example"
+  print(no_vowels(my_string))
