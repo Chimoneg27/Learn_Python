d398d8ba83085ea4e90f57a22f0c60fe23944c03
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-11 16:43:45 +0200

    finish everythin reversed exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-33_everything_reversed/src/everything_reversed.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def everything_reversed(lst):
+  new_list = []
+  for wrd in lst:
+    new_list.append(wrd[::-1])
+  return new_list[::-1]
+
+if __name__ == "__main__":
+  my_list = ["Hi", "there", "example", "one more"]
+  new_list = everything_reversed(my_list)
+  print(new_list)
