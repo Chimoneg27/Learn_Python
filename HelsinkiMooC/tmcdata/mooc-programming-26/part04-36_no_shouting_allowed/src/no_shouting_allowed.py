c2151d6714c9927c90802433aecebd9f13978b9f
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-11 21:46:33 +0200

    finish no shouting allowed exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def no_shouting(arr):
+  new_list = []
+  
+  for str in arr:
+    if str.isupper() == False:
+      new_list.append(str)
+  return new_list
+
+
+if __name__ == "__main__":
+  my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+  pruned_list = no_shouting(my_list)
+  print(pruned_list)
