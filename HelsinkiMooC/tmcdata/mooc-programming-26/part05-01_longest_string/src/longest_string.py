c76bf409fdaea6e81c3d261fde0039f547d28d98
Chimoneg27 <chimonegarvin27@gmail.com>
2026-08-15 20:04:00 +0200

    finish longest string exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part05-01_longest_string/src/longest_string.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def longest(strings: list):
+  long_word = ''
+  for word in strings:
+    if len(word) > len(long_word):
+      long_word = word
+  return long_word
+
+if __name__ == '__main__':
+  strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+  print(longest(strings))
