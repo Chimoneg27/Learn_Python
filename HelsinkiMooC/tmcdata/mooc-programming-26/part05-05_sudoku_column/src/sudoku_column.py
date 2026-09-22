879e291d856035e52fdf71a70cfd074212bef516
Chimoneg27 <chimonegarvin27@gmail.com>
2026-09-22 22:23:05 +0200

    finish sudoku column exercise

--- HelsinkiMooC/tmcdata/mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
+++ HelsinkiMooC/tmcdata/mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,33 @@
 # Write your solution here
+def column_correct(sudoku: list, column_no: int):
+  freq = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+
+  col = []
+  for row in sudoku:
+    col.append(row[column_no])
+
+  for num in col:
+    if num in freq:
+      freq[num] += 1
+  
+  for val in freq.values():
+    if val > 1:
+      return False
+  return True
+
+
+# if __name__ == "__main__":
+#   sudoku = [
+#   [9, 0, 0, 0, 8, 0, 3, 0, 0],
+#   [2, 0, 0, 2, 5, 0, 7, 0, 0],
+#   [0, 2, 0, 3, 0, 0, 0, 0, 4],
+#   [2, 9, 4, 0, 0, 0, 0, 0, 0],
+#   [0, 0, 0, 7, 3, 0, 5, 6, 0],
+#   [7, 0, 5, 0, 6, 0, 4, 0, 0],
+#   [0, 0, 7, 8, 0, 3, 9, 0, 0],
+#   [0, 0, 1, 0, 0, 0, 0, 0, 3],
+#   [3, 0, 0, 0, 0, 0, 0, 0, 2]
+# ]
+
+#   print(column_correct(sudoku, 0))
+#   print(column_correct(sudoku, 1))
